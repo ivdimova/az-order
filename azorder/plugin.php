83c0dccb919b3fe2d21f18da1b914056f8646c14
Ivelina Dimova <iv.dimova@gmail.com>
2023-06-13 03:38:26 +0300

    Update docs

--- azorder/plugin.php
+++ azorder/plugin.php
@@ -1,13 +1,14 @@
 <?php
 /**
- * Plugin Name: A-Z Order 
+ * Plugin Name: A-Z Order.
  * Plugin URI: http:/it.ivdimova.com/
  * Author: ivdimova
- * Description: Simple plugin to display posts in alplhabetical order according to specified post type 
+ * Description: Simple plugin to display posts in alplhabetical order
  * Author URI: http://it.ivdimova.com/
  * Version: 2.0
  * License: GPL2
-*/
+ */
+
 namespace AZOrder;
 
 require_once __DIR__ . '/inc/namespace.php';
